aca49bb02b54928395fe828bd16d2df19d9d2ad1
Andrew <andrew@Andrew.168.42.45>
2022-09-13 13:38:36 +0200

    Fixed deployment bugs

--- pages/[profile].js
+++ pages/[profile].js
@@ -35,9 +35,11 @@ export default function Profile() {
 
    const router = useRouter();
 
+   const { profile } = router.query;
+
    let { data, error} = useSWR(`/api/${profile}`, fetcher)
 
-   const { profile } = router.query;
+   
    if(loading || !loading){
     
    
