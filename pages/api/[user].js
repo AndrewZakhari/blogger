2d891ee48114524c7fb57dc9a0282b91fb9138f8
Andrew <andrew@Andrew.168.42.45>
2022-09-13 14:45:56 +0200

    Fixing deployment bugs

--- pages/api/[user].js
+++ pages/api/[user].js
@@ -7,6 +7,9 @@ export default async function handler(req, res){
             name: req.query.user
         }
     })
-   
+    if(user === null){
+        res.status(404).json({message: "Invalid Username"})
+    }else{
     res.status(200).json(user)
+    }
 }
